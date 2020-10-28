f407c77f409d96965f551616a689a421f42bd67d
Daniel Soto <dasg.1993@gmail.comm>
2020-10-28 18:51:52 -0500

    [feature] - refactor app to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, Fragment } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Emojis, Instructions, Modal } from './components'
 
@@ -8,63 +8,28 @@ const emojis = [
   {emoji: '🤔', name: "thinking face" }
 ]
 
-// const App = () => {
-//   const [visible, setVisible] = useState(false);
-//   const [emojiId, setEmojiId] = useState()
-//   const displayEmojiName = (event) => {
-//     setVisible(true)
-//     setEmojiId(event.target.id)
-//   }
-//   const closeModal = () => {
-//     setVisible(false)
-//   }
-//   return (
-//     <div className="container">
-//       <h1>Hello World!</h1>
-//       <Instructions />
-//       <p>I'm writing JSX</p>
-//       <Emojis data={emojis} onClick={displayEmojiName}/>
-//       <Modal visible={visible} closeModal={closeModal}>
-//         {emojiId}
-//       </Modal>
-//     </div>
-//   );
-// }
-
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      visible: false,
-      emojiId: ''
-    }
-
+const App = () => {
+  const [visible, setVisible] = useState(false);
+  const [emojiId, setEmojiId] = useState()
+  const displayEmojiName = (event) => {
+    setVisible(true)
+    setEmojiId(event.target.id)
   }
-    displayEmojiName = (event) => {
-      this.setState({
-        visible: true,
-        emojiId: event.target.id
-      })
-    }
-    closeModal = () => {
-      this.setState({
-        visible: false
-      })
-    }
-    render(){
-      const { visible, emojiId } = this.state;
-      return (
-        <div className="container">
-          <h1>Hello World!</h1>
-          <Instructions />
-          <p>I'm writing JSX</p>
-          <Emojis data={emojis} onClick={this.displayEmojiName}/>
-          <Modal visible={visible} closeModal={this.closeModal}>
-            {emojiId}
-          </Modal>
-        </div>
-      );
-    }
+  const closeModal = () => {
+    setVisible(false)
+  }
+  return (
+    <div className="container">
+      <h1>Hello World!</h1>
+      <Instructions />
+      <p>I'm writing JSX</p>
+      <Emojis data={emojis} onClick={displayEmojiName}/>
+      <Modal visible={visible} closeModal={closeModal}>
+        {emojiId}
+      </Modal>
+    </div>
+  );
 }
+
 
 export default App;
