7fedf2fe5e25d3a6b50fa6c6b9194218431a24e1
Daniel Soto <dasg.1993@gmail.comm>
2020-10-28 20:57:29 -0500

    [feature] - create card - animal card & add data in constants & install prop-types

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import {data} from './constants/data'
+import {AnimalCard} from './components/Card/AnimalCard'
 import './App.css';
 import { Emojis, Instructions, Modal } from './components'
 
@@ -11,23 +13,53 @@ const emojis = [
 const App = () => {
   const [visible, setVisible] = useState(false);
   const [emojiId, setEmojiId] = useState()
+  const [animalModal, setAnimalModal] = useState(false)
+  const [animalInformation, setAnimalInformation] = useState([])
   const displayEmojiName = (event) => {
     setVisible(true)
     setEmojiId(event.target.id)
   }
   const closeModal = () => {
     setVisible(false)
+    setAnimalModal(false)
+  }
+
+  const showAdditional = (additional) => {
+    const ModalInformation = Object.entries(additional)
+      .map(information => `${information[0]} : ${information[1]}`)
+      .join('\n');
+      setAnimalModal(true)
+      setAnimalInformation(ModalInformation)
   }
   return (
+    <>
     <div className="container">
       <h1>Hello World!</h1>
       <Instructions />
       <p>I'm writing JSX</p>
       <Emojis data={emojis} onClick={displayEmojiName}/>
+      <h1>Animal</h1>
       <Modal visible={visible} closeModal={closeModal}>
         {emojiId}
       </Modal>
+      <Modal visible={animalModal} closeModal={closeModal}>
+          {animalInformation}
+      </Modal>
     </div>
+    <div className="wrapperAnimalCard">
+        {data.map(animal => (
+          <AnimalCard
+            key={animal.name}
+            name={animal.name}
+            additional={animal.additional}
+            diet={animal.diet}
+            scientificName={animal.scientificName}
+            size={animal.size}
+            showAdditional={showAdditional}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
---
+++ src/constants/data.js
@@ -0,0 +1,27 @@
+export const data = [
+    {
+        name: 'Lion',
+        scientificName: 'Pathero leo',
+        size: 140,
+        diet: ['meat']
+    },
+    {
+        name: 'Gorilla',
+        scientificName: 'Gorilla beringei',
+        size: 205,
+        diet: ['plant', 'insects'],
+        additional: {
+            notes: 'This is the eastern gorilla, There is also a western gorilla that is a different species'
+        }
+    },
+    {
+        name: 'Zebra',
+        scientificName: 'Equus quagga',
+        size: 322,
+        diet: ['plants'],
+        additional: {
+            notes: 'There are three different species of zebra.',
+            link: 'https://en.wikipedia.org/wiki/Zebra'
+        }
+    },
+]
