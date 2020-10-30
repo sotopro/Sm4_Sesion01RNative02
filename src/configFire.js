a8b05e0c9d257f07662315ee568ae0dfb20f0bf9
Daniel Soto <dasg.1993@gmail.comm>
2020-10-29 21:48:23 -0500

    [feature] - setup & use enviroment variables in configFirebasev

--- src/configFire.js
+++ src/configFire.js
@@ -1,14 +1,24 @@
 import firebase from 'firebase'
 
 // Your web app's Firebase configuration
+
+const { REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DATABASE_URL,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID
+} = process.env || {}
+
 var firebaseConfig = {
-    apiKey: "AIzaSyBzX8-26w_7TTNjg6wnOkGOVV2I5KkM_ek",
-    authDomain: "semana04-52dd1.firebaseapp.com",
-    databaseURL: "https://semana04-52dd1.firebaseio.com",
-    projectId: "semana04-52dd1",
-    storageBucket: "semana04-52dd1.appspot.com",
-    messagingSenderId: "535986398629",
-    appId: "1:535986398629:web:afff4f40d0103e13c5b44c"
+    apiKey: REACT_APP_API_KEY,
+    authDomain: REACT_APP_AUTH_DOMAIN,
+    databaseURL: REACT_APP_DATABASE_URL,
+    projectId: REACT_APP_PROJECT_ID,
+    storageBucket: REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+    appId: REACT_APP_APP_ID
   };
   // Initialize Firebase
 const configFirebase = firebase.initializeApp(firebaseConfig);
